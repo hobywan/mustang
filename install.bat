47f5030c5a485d73e1cb5bed11f6f41f5c97a97c
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-04-17 18:42:04 +0200

    Minor updates

--- install.bat
+++ install.bat
@@ -1,6 +1,26 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+:: Copyright 2018, Hoby Rakotoarivelo.
+::
+:: Permission is hereby granted, free of charge, to any person obtaining a copy
+:: of this software and associated documentation files (the “Software”),
+:: to deal in the Software without restriction, including without limitation
+:: the rights to use, copy, modify, merge, publish, distribute, sublicense,
+:: and/or sell copies of the Software, and to permit persons to whom the
+:: Software is furnished to do so, subject to the following conditions:
+::
+:: The above copyright notice and this permission notice shall be included in
+:: all copies or substantial portions of the Software.
+::
+:: The Software is provided “as is”, without warranty of any kind, express
+:: or implied, including but not limited to the warranties of merchantability,
+:: fitness for a particular purpose and noninfringement.
+:: In no event shall the authors or copyright holders be liable for any claim,
+:: damages or other liability, whether in an action of contract, tort or
+:: otherwise, arising from, out of or in connection with the software or
+:: the use or other dealings in the Software.
+
 set settings_root=""
 set detecting_path=""
 set colors_dir=""
@@ -26,7 +46,7 @@ goto :eof
     )
     copy /y "!scheme!" "!dest!" >nul
     if %errorlevel% equ 0 (
-      echo 'Mustang scheme successfully installed for "!ide!"'.
+      echo "Mustang scheme successfully installed for !ide!".
     )
   )
 goto :eof
@@ -39,7 +59,7 @@ goto :eof
       call :copy_scheme "%%a", "mustang.clion.icls"
     )
 
-    echo %%a | findstr /i /r /c:"^\.?IdeaC.*$" "^\.?IntelliJ.*$"
+    echo "%%a" | findstr /i /r /c:"^\.?IdeaC.*$" "^\.?IntelliJ.*$"
     if %errorlevel% equ 1 (
       set "found=true"
       call :copy_scheme "%%a", "mustang.idea.icls"
@@ -47,7 +67,7 @@ goto :eof
   )
 
   if not defined "!found!" (
-    echo 'No supported IDE detected'
+    echo "No supported IDE detected"
     exit /b 1
   )
 goto :eof
